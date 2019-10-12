df231307fbf0856427f17d9898396dac00a9d9dc
Anatomica <maksfomin@gmail.com>
2019-10-12 13:31:47 +0300

    Create homework 3.1

--- src/lesson3/Homework/MainClass.java
+++ src/lesson3/Homework/MainClass.java
@@ -1,9 +1,14 @@
 package lesson3.Homework;
+import java.util.HashMap;
 
 public class MainClass {
-
     public static void main(String[] args) {
-
-        
+        String[] words =
+                {"A", "A", "B", "C", "C", "C", "D", "D", "E", "F", "G", "G", "G"};
+        HashMap <String, Integer> arrayWords = new HashMap<>();
+        for (String i : words) {
+            arrayWords.put(i, arrayWords.getOrDefault(i, 0) + 1);
+        }
+        System.out.println(arrayWords);
     }
 }

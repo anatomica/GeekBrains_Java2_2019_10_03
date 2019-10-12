e42743fece0d4ad6830bfe61eee40da35fb60473
Anatomica <maksfomin@gmail.com>
2019-10-12 19:00:16 +0300

    Create homework 3.2

--- src/lesson3/Homework/MainClass.java
+++ src/lesson3/Homework/MainClass.java
@@ -10,5 +10,20 @@ public class MainClass {
             arrayWords.put(i, arrayWords.getOrDefault(i, 0) + 1);
         }
         System.out.println(arrayWords);
+        System.out.println(" ");
+
+        // Задание 3.2
+        PhoneBook phonebook = new PhoneBook();
+        phonebook.add("Макс", "111222");
+        phonebook.add("Олег", "111333");
+        phonebook.add("Витя", "111444");
+        phonebook.add("Рома", "111555");
+        phonebook.add("Ника", "111666");
+        phonebook.add("Ника", "111777");
+
+        phonebook.get("Макс");
+        phonebook.get("Олег");
+        phonebook.get("Ника");
+
     }
 }
--- src/lesson3/Homework/PhoneBook.java
+++ src/lesson3/Homework/PhoneBook.java
@@ -1,4 +1,28 @@
 package lesson3.Homework;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
-public class PhoneBook {
+class PhoneBook {
+    private Map<String, HashSet<String>> phoneBook;
+
+    PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    void add(String name, String phone) {
+        HashSet<String> number = phoneBook.getOrDefault(name, new HashSet<>());
+        number.add (phone);
+        phoneBook.put(name, number);
+    }
+
+    void get(String name) {
+        System.out.println("Имя: " + name + ", номер телефона: " +
+        phoneBook.getOrDefault(name, new HashSet<>()));
+
+        // for (String key : phoneBook.keySet()) {
+        //     String value = phoneBook.get(key);
+        // }
+        // System.out.println(name + " : " + phoneBook.get(name));
+    }
 }

ec6a15c348c52a1110002a97799f4940bbf40b48
Anatomica <maksfomin@gmail.com>
2019-10-19 13:43:55 +0300

    HW 5 & repair furmula, lambda

--- src/lesson5/Homework/MainClass.java
+++ src/lesson5/Homework/MainClass.java
@@ -2,8 +2,8 @@ package lesson5.Homework;
 
 public class MainClass {
 
-    static final int size = 10000000;
-    static final int h = size / 2;
+    private static final int size = 10000000;
+    private static final int h = size / 2;
 
     public static void main(String[] args) {
 
@@ -24,30 +24,23 @@ public class MainClass {
         myThread(arr);
     }
 
-    static void myThread (float[] arr) {
-
+    private static void myThread(float[] arr) {
         long b = System.currentTimeMillis();
-        float[] arr1 = new float[h];
-        float[] arr2 = new float[h];
-        System.arraycopy(arr, 0, arr1, 0, h);
-        System.arraycopy(arr, h, arr2, 0, h);
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < arr1.length; i++) {
-                    arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+        float[] arr1 = new float[h];
+        System.arraycopy(arr, 0, arr1, 0, h);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
         thread1.start();
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < arr2.length; i++) {
-                    arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+        float[] arr2 = new float[h];
+        System.arraycopy(arr, h, arr2, 0, h);
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) ((arr2[i] + h) * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5) * Math.cos(0.4f + (i + h) / 2));
             }
         });
         thread2.start();

fb063a4265f08c30b8272d5ecc18343c6fb5fb35
anatomica <maksfomin@gmail.com>
2019-10-30 01:14:05 +0300

    Lesson 8

---
+++ src/Messenger/Server/gson/ClientListMessage.java
@@ -0,0 +1,18 @@
+package Messenger.Server.gson;
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ClientListMessage {
+
+    public List<String> online;
+
+    public  String toJson() {
+        return new Gson().toJson(this);
+    }
+
+    public static ClientListMessage fromJson(String json) {
+        return new Gson().fromJson(json, ClientListMessage.class);
+    }
+}
